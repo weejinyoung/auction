c3865327a3dfbb4a11009b1b63dae69ee88adabd
Yang Seok Jin <tjrwls08088@naver.com>
2025-04-01 12:46:27 +0900

    add: get Auction with rating

--- src/main/java/com/ourfantasy/auction/auction/repository/AuctionCustomRepository.java
+++ src/main/java/com/ourfantasy/auction/auction/repository/AuctionCustomRepository.java
@@ -2,10 +2,12 @@ package com.ourfantasy.auction.auction.repository;
 
 import com.ourfantasy.auction.auction.model.Auction;
 import com.ourfantasy.auction.item.model.ItemCategory;
+import com.querydsl.core.Tuple;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface AuctionCustomRepository {
     Page<Auction> findLatestOpenedAuctions(Pageable pageable);
     Page<Auction> findNearestClosingAuctionsByCategory(Pageable pageable, ItemCategory itemCategory);
+    Page<Tuple> findNearestClosingAuctionsByCategoryWithRating(Pageable pageable, ItemCategory itemCategory);
 }
