a043983a0f4fb0953c7940dc7ffadd172e0a18e4
Yang Seok Jin <tjrwls08088@naver.com>
2025-04-01 12:46:56 +0900

    add: get auction findNearestClosingAuctionsWithRating

--- src/main/java/com/ourfantasy/auction/auction/repository/AuctionCustomRepositoryImpl.java
+++ src/main/java/com/ourfantasy/auction/auction/repository/AuctionCustomRepositoryImpl.java
@@ -5,7 +5,10 @@ import com.ourfantasy.auction.auction.model.AuctionStatus;
 import com.ourfantasy.auction.auction.model.QAuction;
 import com.ourfantasy.auction.item.model.ItemCategory;
 import com.ourfantasy.auction.item.model.QItem;
+import com.ourfantasy.auction.rating.model.QItemRating;
+import com.ourfantasy.auction.rating.model.QUserRating;
 import com.ourfantasy.auction.user.model.QUser;
+import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -14,6 +17,8 @@ import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+import static com.querydsl.jpa.JPAExpressions.select;
 
 @Repository
 public class AuctionCustomRepositoryImpl extends QuerydslRepositorySupport implements AuctionCustomRepository {
@@ -38,6 +43,8 @@ public class AuctionCustomRepositoryImpl extends QuerydslRepositorySupport imple
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
+
+
 
         Long total = from(auction)
                 .select(auction.countDistinct())
@@ -72,5 +79,42 @@ public class AuctionCustomRepositoryImpl extends QuerydslRepositorySupport imple
                 .fetchOne();
 
         return new PageImpl<>(content, pageable, total != null ? total : 0L);
+    }
+
+    @Override
+    public Page<Tuple> findNearestClosingAuctionsByCategoryWithRating(Pageable pageable, ItemCategory itemCategory) {
+        QAuction auction = QAuction.auction;
+        QUser user = QUser.user;
+        QItem item = QItem.item;
+        QItemRating itemRating = QItemRating.itemRating;
+        QUserRating userRating = QUserRating.userRating;
+
+        BooleanExpression conditions = auction.status.eq(AuctionStatus.ACTIVE)
+                .and(item.category.eq(itemCategory));
+
+        List<Tuple> newContent =
+                select(
+                        auction,
+                        itemRating.score.avg().coalesce(0.0),   // 아이템 평균 평점
+                        userRating.score.avg().coalesce(0.0)    // 판매자 평균 평점
+                ).from(auction)
+                        .leftJoin(auction.item, item).fetchJoin()
+                        .leftJoin(auction.cosigner, user).fetchJoin()
+                        .leftJoin(itemRating).on(itemRating.item.eq(item))        // 아이템 평점 조인
+                        .leftJoin(userRating).on(userRating.ratee.eq(user))        // 판매자 평점 조인
+                        .where(conditions)
+                        .orderBy(auction.createdAt.desc())
+                        .offset(pageable.getOffset())
+                        .limit(pageable.getPageSize())
+                        .fetch();
+
+
+        Long total = from(auction)
+                .join(auction.item, item)
+                .select(auction.countDistinct())
+                .where(conditions)
+                .fetchOne();
+
+        return new PageImpl<>(newContent, pageable, total != null ? total : 0L);
     }
 }
