1887a1cdec86877344afebb329035ef3c388a01a
Yang Seok Jin <tjrwls08088@naver.com>
2025-04-01 12:47:19 +0900

    modify: add entity annotation

--- src/main/java/com/ourfantasy/auction/rating/model/ItemRating.java
+++ src/main/java/com/ourfantasy/auction/rating/model/ItemRating.java
@@ -8,6 +8,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ItemRating extends BaseTimeEntity {
--- src/main/java/com/ourfantasy/auction/rating/model/UserRating.java
+++ src/main/java/com/ourfantasy/auction/rating/model/UserRating.java
@@ -7,6 +7,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserRating extends BaseTimeEntity {
