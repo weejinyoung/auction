18dd9f57c304d7deca2889a3fc837a897b95f2b6
Yang Seok Jin <tjrwls08088@naver.com>
2025-04-03 00:39:47 +0900

    remove: rating model

--- src/main/java/com/ourfantasy/auction/rating/repository/UserRatingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ourfantasy.auction.rating.repository;
-
-import com.ourfantasy.auction.rating.model.UserRating;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRatingRepository extends JpaRepository<UserRating, Long> {
-}
